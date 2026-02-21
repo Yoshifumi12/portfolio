842aed304d212fa02f35e1d04aae93d9a468a386
Yoshifumi12 <Yoshifumisuzuki81194@gmail.com>
2026-02-21 22:21:31 +0800

    feat: initial setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  prettier,
+  {
+    rules: {
+      'react-hooks/exhaustive-deps': 'off',
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: 'Reference|^_',
+          varsIgnorePattern: 'Reference|^_',
+          caughtErrorsIgnorePattern: 'Reference|^_',
+        },
+      ],
+    },
+  },
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
 
-export default eslintConfig;
+export default eslintConfig
