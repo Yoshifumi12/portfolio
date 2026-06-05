781fafb9f90495f0e9b9990f0e66aae7d30f7352
Yoshifumi12 <Yoshifumisuzuki81194@gmail.com>
2026-06-05 22:26:18 +0800

    feat: add projects data

---
+++ app/constants/projectsData.ts
@@ -0,0 +1,92 @@
+import { experienceDataType } from '../types/experienceData'
+
+export const projectsData: experienceDataType[] = [
+  {
+    windowTitle: 'school-project',
+    defaultPosition: { x: 80, y: 60 },
+    size: { width: 700, height: 430 },
+    title: 'Savy: AI-Powered Interview Practice Mobile Application',
+    company: 'University Capstone Project - Central Philippine University',
+    description: [
+      'Collaborated in a 2-person team using Agile scrum to design, build, and test a mobile Android app that simulates personalized job interviews through AI-driven question generation and performance feedback.',
+      'Developed core features: parsing of resumes and job descriptions to generate custom interview questions, a virtual interviewer with voice interaction, a self-recording mode, and multi-metric feedback and ratings (including relevance, estimated eye contact, grammar, speaking pace, and filler words) based on IELTS speaking descriptors.',
+      'Implemented tokenization to ensure the security of the APIs.',
+      'Delivered the final product with documentation that includes entity-relationship diagrams, use cases, and activity diagrams.',
+    ],
+    badge: 'March 2024 - January 2025',
+    techStack: [
+      {
+        name: 'TypeScript',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+      },
+      {
+        name: 'Expo',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/expo/expo-original.svg',
+        style: { filter: 'invert(100%) sepia(100%) grayscale(100%)' },
+      },
+      {
+        name: 'FastAPI',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg',
+      },
+      {
+        name: 'Supabase',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/supabase/supabase-original.svg',
+      },
+      {
+        name: 'Github',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg',
+        style: { filter: 'invert(100%) sepia(100%) grayscale(100%)' },
+      },
+    ],
+  },
+  {
+    windowTitle: 'school-project',
+    defaultPosition: { x: 800, y: 380 },
+    size: { width: 640, height: 357 },
+    title: 'Crowdfunding & Content Management Platform for Global Shapers - Iloilo',
+    company: 'University Project - Central Philippine University',
+    description: [
+      'Designed and implemented a cloud-hosted relational database, including its schema and deployment.',
+      'Developed backend request handlers with end-to-end type safety for efficient frontend-backend communication.',
+      'Ensured robust data handling and seamless integration with the frontend.',
+      'Collaborated with team members on code reviews, task coordination, and system integration',
+      'worked closely with the client to gather requirements and tailor solutions.',
+    ],
+    badge: 'September 2023 -  March 2024',
+    techStack: [
+      {
+        name: 'TypeScript',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+      },
+      {
+        name: 'React',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+      },
+      {
+        name: 'Next.js',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg',
+      },
+      {
+        name: 'Tailwind CSS',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg',
+      },
+      {
+        name: 'tRPC',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/trpc/trpc-original.svg',
+      },
+      {
+        name: 'Prisma',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/prisma/prisma-original.svg',
+      },
+      {
+        name: 'PostgreSQL',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+      },
+      {
+        name: 'Github',
+        icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg',
+        style: { filter: 'invert(100%) sepia(100%) grayscale(100%)' },
+      },
+    ],
+  },
+]
